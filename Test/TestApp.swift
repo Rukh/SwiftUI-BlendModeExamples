71e3969ac7f0cef5f1d39de8aa43d71437192b5f
Gulyagin Dmitry <gulyagin@mail.com>
2023-02-22 18:52:45 +0200

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Dmitry Gulyagin on 22/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
